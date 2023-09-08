78b2a0a2c72fb10a5511b75807da0967cbe778e7
sushantapatra <development.experiment@gmail.com>
2023-09-08 23:23:02 +0530

    Google clone Ui completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		domains: ["pngimg.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+import Header from "@/components/Header";
 import Image from "next/image";
+import SearchInput from '@/components/SearchInput'
 
 export default function Home() {
 	return (
 		<>
-			<h1>Welcome to Google Chrome</h1>
+			<Header />
+			<div className="flex flex-col items-center mt-12">
+				<Image
+					src="https://pngimg.com/uploads/google/small/google_PNG19642.png"
+					width={300}
+					height={100}
+					alt="Picture of the author"
+				/>
+				<SearchInput/>
+			</div>
 		</>
 	);
 }
